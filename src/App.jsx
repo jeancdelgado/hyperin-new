1163b1bd8ebc47cdd1757b8bff1b0ef42d29aed7
rahulmoral <rahulmoral00@gmail.com>
2024-04-08 22:48:38 +0600

    Error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import "./App.scss"
 import {
-  Achievements,
-  // Announcements,
-  CallToAction,
-  DemoRequest,
-  Footer,
-  Hero,
-  Navbar,
-  PowerTool,
-  PowerTools,
-  Reviews,
-  TrustedUsers,
-  WhyHyperIn,
+	Achievements,
+	// Announcements,
+	CallToAction,
+	DemoRequest,
+	Footer,
+	Hero,
+	Navbar,
+	PowerTool,
+	PowerTools,
+	Reviews,
+	TrustedUsers,
+	WhyHyperIn,
 } from "./components"
 
 import shoppingImage from "./assets/HyperIn_Shopping.jpg"
@@ -19,76 +19,53 @@ import Reservation_Calendar from "./assets/Reservation_Calendar.webp"
 import hyperin_Connect from "./assets/hyperin_Connect.webp"
 import { useEffect, useState } from "react"
 
-const powerToolsList = [
-  {
-    title: "Manage",
-    description:
-      "An integrated tool to administrate all your tenants and consumer information and communication as well as sales reporting, contracts, marketing, footfall and all other key numbers and statistics in just one place.",
-    link: "",
-    image: shoppingImage,
-  },
-  {
-    title: "Monetize",
-    description:
-      "Sell all the advertising and promotional spaces of your shopping mall online. Whether on your digital screens, speciality leasing places, voice commercials or websites, you will generate more revenue.",
-    link: "",
-    image: Reservation_Calendar,
-  },
-  {
-    title: "Connect",
-    description:
-      "Targeting the right consumer traffic with the right type of message on the web, on the location wayfinding and, on mobile loyalty app gives you a competitive advantage across your retail real estate.",
-    link: "",
-    image: hyperin_Connect,
-  },
-]
-
 function App() {
-  const [isVisible, setIsVisible] = useState(true)
-  const [showMenu, setShowMenu] = useState(false)
-  
-  let lastScrollY = window.scrollY
+	const [isVisible, setIsVisible] = useState(true)
+	const [showMenu, setShowMenu] = useState(false)
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > lastScrollY) {
-        setIsVisible(false) // Scroll down, hide the header
-      } else {
-        setIsVisible(true) // Scroll up, show the header
-      }
-      lastScrollY = window.scrollY
-    }
+	let lastScrollY = window.scrollY
 
-    window.addEventListener("scroll", handleScroll)
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY > lastScrollY) {
+				setIsVisible(false) // Scroll down, hide the header
+			} else {
+				setIsVisible(true) // Scroll up, show the header
+			}
+			lastScrollY = window.scrollY
+		}
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll)
-    }
-  }, [])
-  
+		window.addEventListener("scroll", handleScroll)
 
-  return (
-    <div id="bodyClone">
-      <header className={`${isVisible || showMenu? 'visible': 'hidden'}`}>
-        {/* <Announcements /> */}
-        <Navbar showMenu={showMenu} setShowMenu={setShowMenu}/>
-      </header>
-      <main>
-        <Hero />
-        <TrustedUsers />
-        <Achievements />
-        <PowerTools />
-        {powerToolsList.map(tool => (
-          <PowerTool key={tool.title} {...tool} />
-        ))}
-        <CallToAction />
-        <Reviews />
-        <WhyHyperIn />
-        <DemoRequest />
-      </main>
-      <Footer />
-    </div>
-  )
+		return () => {
+			window.removeEventListener("scroll", handleScroll)
+		}
+	}, [])
+
+
+	return (
+		<div id="bodyClone">
+			<header className={`${isVisible || showMenu? 'visible': 'hidden'}`}>
+				{/* <Announcements /> */}
+				<Navbar showMenu={showMenu} setShowMenu={setShowMenu}/>
+			</header>
+			<main>
+				<Hero />
+				<TrustedUsers />
+				<Achievements />
+				{/*<PowerTools />*/}
+				{/*{powerToolsList.map(tool => (*/}
+				{/*	<PowerTool key={tool.title} {...tool} />*/}
+				{/*))}*/}
+				<PowerTool />
+				<CallToAction />
+				<Reviews />
+				<WhyHyperIn />
+				<DemoRequest />
+			</main>
+			<Footer />
+		</div>
+	)
 }
 
 export default App
--- src/components/PowerTool/index.jsx
+++ src/components/PowerTool/index.jsx
@@ -1,22 +1,97 @@
 /* eslint-disable react/prop-types */
+import shoppingImage from "../../assets/HyperIn_Shopping.jpg";
+import Reservation_Calendar from "../../assets/Reservation_Calendar.webp";
+import hyperin_Connect from "../../assets/hyperin_Connect.webp";
+import manage from "../../assets/HyperIn_Manage.svg";
+import monetize from "../../assets/HyperIn_Monetize.svg";
+import connect from "../../assets/HyperIn_Connect.svg";
+
+import { Link, Element } from 'react-scroll';
+
 export const PowerTool = ({title, description, link, image}) => {
-  return (
-    <div id="powerTool" >
-      <section id={title} className="powertool-section">
-        <div className="container">
-          <div className="main_mange">
-            <div className="mange_text">
-            <h2 className="green">{title}</h2>
-            <p>{description}</p>
-            <a href={link} className="readmore" >Read more</a>
-            </div>
-            <div className="mange_img">
-              <img src={image} />
-            </div>
-          </div>
-        </div>
-      </section>
-      <div className="border-botom"></div>
-    </div>
-  )
+
+	const powerToolsList = [
+		{
+			title: "Manage",
+			description:
+				"An integrated tool to administrate all your tenants and consumer information and communication as well as sales reporting, contracts, marketing, footfall and all other key numbers and statistics in just one place.",
+			link: "",
+			image: shoppingImage,
+		},
+		{
+			title: "Monetize",
+			description:
+				"Sell all the advertising and promotional spaces of your shopping mall online. Whether on your digital screens, speciality leasing places, voice commercials or websites, you will generate more revenue.",
+			link: "",
+			image: Reservation_Calendar,
+		},
+		{
+			title: "Connect",
+			description:
+				"Targeting the right consumer traffic with the right type of message on the web, on the location wayfinding and, on mobile loyalty app gives you a competitive advantage across your retail real estate.",
+			link: "",
+			image: hyperin_Connect,
+		},
+	]
+
+	return (
+		<div className="d-flex flex-column">
+			<div id="powerTools">
+				<section className="service_section">
+					<div className="container">
+						<h2 className="alone-heading">Powerful alone. And even better together.</h2>
+						<div className="main-services">
+							<Link className="services-card-1" to="Manage" smooth={true} duration={500} spy={false}>
+								<div className="Manage_img">
+									<img alt="Manage" src={manage} />
+								</div>
+								<span>Manage</span>
+								<p>Increase efficiency. Reduce manual work. </p>
+							</Link>
+							{/* 2 */}
+							<Link className="services-card-1" to="Monetize" smooth={true} duration={500} spy={false}>
+								<div className="Manage_img">
+									<img alt="Monetize" src={monetize} />
+								</div>
+								<span>Monetize</span>
+								<p>Increase profits. Create new advertisement sales. </p>
+							</Link>
+							{/* 3 */}
+							<Link className="services-card-1" to="Connect" smooth={true} duration={500} spy={false}>
+								<div className="Manage_img">
+									<img alt={`Connect`} src={connect} />
+								</div>
+								<span>Connect</span>
+								<p>Increase footfall. Multi-channel communications. </p>
+							</Link>
+						</div>
+					</div>
+				</section>
+				<div className="border-botom"></div>
+			</div>
+			<div className={`d-flex flex-column`}>
+				{powerToolsList.map(item => (
+					<div id="powerTool">
+						<Element name={item.title}>
+							<section id={item.title} className="powertool-section">
+								<div className="container">
+									<div className="main_mange">
+										<div className="mange_text">
+											<h2 className="green">{item.title}</h2>
+											<p>{item.description}</p>
+											<a href={item.link} className="readmore" >Read more</a>
+										</div>
+										<div className="mange_img">
+											<img alt={item.title} src={item.image} />
+										</div>
+									</div>
+								</div>
+							</section>
+							<div className="border-botom"></div>
+						</Element>
+					</div>
+				))}
+			</div>
+		</div>
+	)
 }
